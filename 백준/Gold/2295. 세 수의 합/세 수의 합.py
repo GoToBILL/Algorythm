9e0b99d390a037bc915f1facbe8274336f72f467
GoToBILL <96755568+GoToBILL@users.noreply.github.com>
2023-11-06 12:03:27 +0900

    [Gold IV] Title: 세 수의 합, Time: 292 ms, Memory: 65940 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -0,0 +1,24 @@
+import sys
+
+def check():
+    global answer
+    
+    for i in range(N-1, -1, -1):
+        for j in range(i+1):
+            if arr[i]-arr[j] in arr_sum:
+                answer = arr[i]
+                return
+
+input = sys.stdin.readline
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+arr.sort()
+
+arr_sum = set()
+for x in arr:
+    for y in arr:
+        arr_sum.add(x+y)
+
+answer = 0
+check()
+print(answer)
