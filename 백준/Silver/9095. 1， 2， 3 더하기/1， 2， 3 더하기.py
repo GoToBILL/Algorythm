40bddaf5df09a20e123e75d0ba42719d90a7a960
GoToBILL <96755568+GoToBILL@users.noreply.github.com>
2024-01-16 17:28:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 34000 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+lists = [int(input()) for _ in range(n)]
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4,11):
+    dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
+for i in range(n):
+    print(dp[lists[i]])
+    
