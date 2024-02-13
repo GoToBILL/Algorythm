aabc99b076b66bb763711dc0dbd856ed36b8e23a
GoToBILL <96755568+GoToBILL@users.noreply.github.com>
2024-02-13 12:58:39 +0900

    [Silver V] Title: 숫자 카드, Time: 1052 ms, Memory: 116040 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,31 +1,22 @@
 import sys
 from collections import deque
+from collections import defaultdict
+from itertools import permutations as pt
+from itertools import combinations
+from itertools import product
+from itertools import combinations_with_replacement
+import bisect
 sys.setrecursionlimit(10**6)
-
-def is_here(a):
-    start = 0
-    last = len(lists)-1
-    while start <= last:
-        mid = (start+last) // 2
-        if lists[mid] > a:
-            last = mid - 1
-        elif lists[mid] < a:
-            start = mid + 1
-        else:
-            return 1
-    return 0
 
 input = sys.stdin.readline
 n = int(input())
 lists = list(map(int,input().split()))
 lists.sort()
-
-
 m = int(input())
-find_list = list(map(int,input().split()))
-
-for num in find_list:
-    if is_here(num):
-        print(1,end=' ')
+want_find = list(map(int,input().split()))
+for num in want_find:
+    a = bisect.bisect_left(lists,num)
+    if a != len(lists) and lists[a] == num:
+        print(1,end=" ")
     else:
-        print(0,end=' ')
+        print(0,end=" ")
